a47152786df0ccd76397da247a971e9231ff835c
ijh1298 <imjh0417@gmail.com>
2024-12-12 18:07:18 +0900

    chore: 카테고리 출력 수정

--- src/main/kotlin/menu/view/OutputView.kt
+++ src/main/kotlin/menu/view/OutputView.kt
@@ -6,7 +6,7 @@ class OutputView {
     fun printResult(coaches: List<Coach>) {
         println("메뉴 추천 결과입니다.")
         println("[ 구분 | 월요일 | 화요일 | 수요일 | 목요일 | 금요일 ]")
-        println("[ 카테고리 | %s ]")
+        println("[ 카테고리 | ${coaches[0].weeklyMenus.joinToString(" | ") { it.category }} ]")
         coaches.forEach { println(it.weeklyMenusToString()) }
     }
 }
