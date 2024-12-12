25b690254f0e0b5f9044513130e0241808da5aa4
ijh1298 <imjh0417@gmail.com>
2024-12-12 16:23:15 +0900

    feat: 서비스 Parsing Service 내 splitByComma 메소드 추가

--- src/main/kotlin/menu/service/ParsingService.kt
+++ src/main/kotlin/menu/service/ParsingService.kt
@@ -3,5 +3,7 @@ package menu.service
 import menu.model.Coach
 
 object ParsingService {
+    fun splitByComma(input: String): List<String> = input.split(',').map { it }
+
     fun stringToCoaches(input: String): List<Coach> = input.split(',').map { name -> Coach(name) }
 }
