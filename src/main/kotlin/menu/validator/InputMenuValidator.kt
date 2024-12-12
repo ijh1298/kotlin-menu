c4b1cc4df65f7623ec0640afea52de35fa8d7bb8
ijh1298 <imjh0417@gmail.com>
2024-12-12 16:30:51 +0900

    feat: InputMenuValidator 추가

--- src/main/kotlin/menu/validator/InputMenuValidator.kt
+++ src/main/kotlin/menu/validator/InputMenuValidator.kt
@@ -1,4 +1,21 @@
 package menu.validator
 
-class InputMenuValidator {
+import menu.service.MenuService
+import menu.service.ParsingService
+import menu.utils.Error
+
+class InputMenuValidator(
+    private val inputMenus: String
+) {
+    private lateinit var parsedMenus: List<String>
+
+    fun validate() {
+        parsedMenus = ParsingService.splitByComma(inputMenus)
+        require(isInRange()) { Error.MENU_NUMBER_NOT_IT_RANGE.msg }
+        require(isMenuAllIncluded()) { Error.MENU_NOT_EXIST.msg }
+    }
+
+    private fun isInRange() = parsedMenus.size in 0..2
+
+    private fun isMenuAllIncluded() = parsedMenus.all { MenuService.isExistedMenu(it) }
 }
