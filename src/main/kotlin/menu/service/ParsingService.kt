dbadd1153c003eef1451618d18add0289a1114cf
ijh1298 <imjh0417@gmail.com>
2024-12-12 16:37:19 +0900

    feat: ParsingService 내 stringToMenus 메소드 추가

--- src/main/kotlin/menu/service/ParsingService.kt
+++ src/main/kotlin/menu/service/ParsingService.kt
@@ -1,9 +1,16 @@
 package menu.service
 
 import menu.model.Coach
+import menu.model.Menu
 
 object ParsingService {
     fun splitByComma(input: String): List<String> = input.split(',').map { it }
 
-    fun stringToCoaches(input: String): List<Coach> = input.split(',').map { name -> Coach(name) }
+    fun stringToCoaches(inputCoachNames: String): List<Coach> {
+        return inputCoachNames.split(',').map { name -> Coach(name) }
+    }
+
+    fun stringToMenus(inputMenuNames: String): List<Menu> {
+        return inputMenuNames.split(',').map { name -> Menu(name, MenuService.getCategoryByName(name)) }
+    }
 }
