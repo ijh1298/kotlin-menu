7c0292e704b077ee86bb81bc2e9d6c0cea1799dc
ijh1298 <imjh0417@gmail.com>
2024-12-12 19:21:19 +0900

    feat: OutputView 내 시작 출력 함수 추가

--- src/main/kotlin/menu/view/OutputView.kt
+++ src/main/kotlin/menu/view/OutputView.kt
@@ -3,10 +3,15 @@ package menu.view
 import menu.model.Coach
 
 class OutputView {
+    fun printStartMessage() {
+        println("점심 메뉴 추천을 시작합니다.")
+    }
+
     fun printResult(coaches: List<Coach>) {
         println("메뉴 추천 결과입니다.")
         println("[ 구분 | 월요일 | 화요일 | 수요일 | 목요일 | 금요일 ]")
         println("[ 카테고리 | ${coaches[0].weeklyMenus.joinToString(" | ") { it.category }} ]")
         coaches.forEach { println(it.weeklyMenusToString()) }
+        println("\n추천을 완료했습니다.")
     }
 }
